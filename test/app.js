6a9c43d16686f75bf300e57013ff65910303aa84
Adam Powers <apowers@ato.ms>
2016-02-10 17:01:19 -0800

    initial working tests

--- test/app.js
+++ test/app.js
@@ -1,19 +1,51 @@
-'use strict';
+//'use strict';
 var path = require('path');
-var assert = require('yeoman-assert');
+// var assert = require("chai");
+var fsassert = require('yeoman-assert');
 var helpers = require('yeoman-generator').test;
 
-describe('generator-fido-release:app', function () {
-  before(function (done) {
+var default_prompts = {
+  cont: true,
+  test: false,
+  specset: "uaf",
+  specversion: '1.0',
+  specstatus: 'wd',
+  tagdate: "20160202",
+  tagaddon: ""
+};
+var expectedFolder = "fido-uaf-v1.0-wd-20160202";
+var expectedFolderPath;
+
+var default_arguments = {
+
+};
+
+describe('generator-fido-release:app', function() {
+
+  before(function(done) {
+    // running the generator will do a `git clone` which may be slow, based on your location and connection speed
+    this.timeout(120000);
     helpers.run(path.join(__dirname, '../generators/app'))
-      .withOptions({someOption: true})
-      .withPrompts({someAnswer: true})
-      .on('end', done);
+      // .withOptions({})
+      .withPrompts(default_prompts)
+      .on('end', function() {
+        expectedFolderPath = path.join(process.cwd(), "../" + expectedFolder);
+        done();
+      });
   });
 
-  it('creates files', function () {
-    assert.file([
-      'dummyfile.txt'
+  this.timeout(2000);
+
+  it('creates folder ' + expectedFolder, function() {
+    fsassert.file([
+      expectedFolderPath
+    ]);
+  });
+
+  it('creates files', function() {
+    var file = path.join(expectedFolderPath, 'fido-glossary.html');
+    fsassert.file([
+      file
     ]);
   });
 });
